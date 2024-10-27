0b2410cc0fceba0c4d48074cc61a307a52ec58ef
LeyLopez <leidislopez2017@gmail.com>
2024-10-27 00:08:12 -0500

    Updates

--- build/app/historialEstadosReservas/repository/HistorialReservaSQL.js
+++ build/app/historialEstadosReservas/repository/HistorialReservaSQL.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SQL_HISTORIAL_ESTADO_RESERVA = void 0;
 exports.SQL_HISTORIAL_ESTADO_RESERVA = {
-    GET_ALL: "SELECT * FROM Historial_estados_reserva",
+    GET_ALL: "SELECT * FROM Historial_estados_reservas",
     ADD: "INSERT INTO Historial_estados_reserva (fecha_cambio_estado, id_reserva, id_estado) VALUES ($1, $2, $3) \
     RETURNING id_historial_estado_reserva",
-    HOW_MANY: "SELECT COUNT(*) FROM Historial_estados_reserva",
-    HOW_MANY_RESERVA_ESTADO: "SELECT COUNT(*) FROM Historial_estados_reserva WHERE id_reserva = $1 AND id_estado = $2",
-    DELETE: "DELETE FROM Historial_estados_reserva WHERE id_historial_estado_reserva = $1",
-    UPDATE: "UPDATE Historial_estados_reserva SET fecha_cambio_estado = $1, id_reserva = $2, id_estado = $3 \
+    HOW_MANY: "SELECT COUNT(*) FROM Historial_estados_reservas",
+    HOW_MANY_RESERVA_ESTADO: "SELECT COUNT(*) FROM Historial_estados_reservas WHERE id_reserva = $1 AND id_estado = $2",
+    DELETE: "DELETE FROM Historial_estados_reservas WHERE id_historial_estado_reserva = $1",
+    UPDATE: "UPDATE Historial_estados_reservas SET fecha_cambio_estado = $1, id_reserva = $2, id_estado = $3 \
     WHERE id_historial_estado_reserva = $4 RETURNING id_historial_estado_reserva",
 };
--- src/app/historialEstadosPrestamos/repository/HistorialPrestamoSQL.ts
+++ src/app/historialEstadosPrestamos/repository/HistorialPrestamoSQL.ts
@@ -1,17 +1,17 @@
 export const SQL_HISTORIAL_ESTADO_PRESTAMO ={
 
-    GET_ALL: "SELECT * FROM Historial_estados_prestamo",
+    GET_ALL: "SELECT * FROM Historial_estados_prestamos",
 
-    ADD: "INSERT INTO Historial_estados_prestamo (fecha_cambio_estado, id_prestamo, id_estado) VALUES ($1, $2, $3) \
+    ADD: "INSERT INTO Historial_estados_prestamos (fecha_cambio_estado, id_prestamo, id_estado) VALUES ($1, $2, $3) \
     RETURNING id_historial_estado_prestamo",
 
-    HOW_MANY: "SELECT COUNT(*) FROM Historial_estados_prestamo",
+    HOW_MANY: "SELECT COUNT(*) FROM Historial_estados_prestamos",
 
-    HOW_MANY_PRESTAMO_ESTADO: "SELECT COUNT(*) FROM Historial_estados_prestamo WHERE id_prestamo = $1 AND id_estado = $2",
+    HOW_MANY_PRESTAMO_ESTADO: "SELECT COUNT(*) FROM Historial_estados_prestamos WHERE id_prestamo = $1 AND id_estado = $2",
 
-    DELETE: "DELETE FROM Historial_estados_prestamo WHERE id_historial_estado_prestamo = $1",
+    DELETE: "DELETE FROM Historial_estados_prestamos WHERE id_historial_estado_prestamo = $1",
 
-    UPDATE: "UPDATE Historial_estados_prestamo SET fecha_cambio_estado = $1, id_prestamo = $2, id_estado = $3 \
+    UPDATE: "UPDATE Historial_estados_prestamos SET fecha_cambio_estado = $1, id_prestamo = $2, id_estado = $3 \
     WHERE id_historial_estado_prestamo = $4 RETURNING id_historial_estado_prestamo",
 
     
